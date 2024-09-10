42d668c1c93068ea73d7b9634eeaf791a18b8eb9
krunalkumar0354 <krunalkumar@zeni.ai>
2024-09-10 13:04:43 +0530

    Null or Empty notes part handled.

--- Generate Summary.py
+++ Generate Summary.py
@@ -2,11 +2,23 @@ import os, requests
 
 def main(event):
   token = os.getenv("ChatGPT")
-  notes = event.get('inputFields').get('notes')
-  meetings = event.get('inputFields').get('meeting_notes')
-  preText = "I have extracted following ticket information i.e. ticket notes and meetings notes. I want you to summarise why this is churn?"
+  cnotes = event.get('inputFields').get('cNotes')
+  tnotes = event.get('inputFields').get('tNotes')
+  mnotes = event.get('inputFields').get('mNotes')
+  preText = "I have extracted following ticket information i.e. company notes,ticket notes and meetings notes. I want you to summarise why this is churn?"
   postText = " Just write the summary and do not enter anything like here's the summary. go straight into summary."
-  finalText = preText + "Ticket Notes ->" + notes + "Meeting Notes ->" + meetings + postText
+  if cnotes == None:
+    finalText = preText + "Company Notes ->" + "No Company Notes."
+  else:
+    finalText = preText + "Company Notes ->" + cnotes
+  if tnotes == None:
+    finalText = finalText + "Ticket Notes ->" + "No Ticket Notes."
+  else:
+    finalText = finalText + "Ticket Notes ->" + tnotes
+  if mnotes == None:
+    finalText = finalText + "Meeting Notes ->" + "No Meeting Notes." + postText
+  else:
+    finalText = finalText + "Meeting Notes ->" + mnotes + postText
   openai_endpoint = 'https://api.openai.com/v1/chat/completions'
   headers = {
         'Authorization': f'Bearer {token}',
