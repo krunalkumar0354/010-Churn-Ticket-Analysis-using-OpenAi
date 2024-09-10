c194fc831d1fed1075574849a76b561b37e43d7a
krunalkumar0354 <krunalkumar@zeni.ai>
2024-09-11 04:06:31 +0530

    correctness also added to make sure the answer is more accurate.

--- Recommended Churn Reason.py
+++ Recommended Churn Reason.py
@@ -3,9 +3,10 @@ import os, requests, time
 def main(event):
   token = os.getenv("ChatGPT")
   summary = event.get('inputFields').get('conclusion')
+  correctness = event.get('inputFields').get('Correctnss')
   preText = "Select one recommended churn reason from these options: Company Shut Down, Lack of Funding, Pricing Issues, Service Issues, ICP Misfit, Hired In-House Team, Moved to NetSuite, Moved to Competitor, Zeni's Decision, Company Sold, Controller Issue."
   postText = "Here's the summary, by using which you can select the recommended churn reason as mentioned earlier. Summary = " + summary
-  finalText = preText + postText + "do not prefix anything like Recommended Churn Reason, instead just mention the reason."
+  finalText = preText + postText + "Please take this correctness into account while generating recommended churn reason." + correctness + "do not prefix anything like Recommended Churn Reason, instead just mention the reason."
   openai_endpoint = 'https://api.openai.com/v1/chat/completions'
   headers = {
         'Authorization': f'Bearer {token}',
@@ -23,7 +24,6 @@ def main(event):
   }
   time.sleep(5)
   response = requests.post(openai_endpoint, headers=headers, json=data)
-  print(response.status_code)
   reason = response.json().get('choices', [{}])[0].get('message', {}).get('content', '')
   return {
     "outputFields": {
